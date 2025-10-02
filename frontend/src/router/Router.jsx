354dc7e73703bcfee64f2d0bdb6ed692fe6b1e17
StefanMogren <stefan_mogren@hotmail.com>
2025-10-02 11:48:44 +0200

    Removed unused LoginPage and RegisterPage

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -3,9 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // ----- Pages -----
 import HomePage from "../pages/HomePage/HomePage.jsx";
 import NoPage from "../pages/NoPage/NoPage.jsx";
-import LoginPage from "../pages/LoginPage/LoginPage.jsx";
 import PostsByUserPage from "../pages/PostsByUserPage/PostsByUserPage.jsx";
-import RegisterPage from "../pages/RegisterPage/RegisterPage.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 
 // ----- Components -----
@@ -20,15 +18,12 @@ export const Router = () => {
 					<HomePageLink />
 					<Routes>
 						<Route path='/' element={<HomePage />} />
-						<Route path='/login' element={<LoginPage />} />
-						<Route path='/register' element={<RegisterPage />} />
 						<Route path='/posts-by/:username' element={<PostsByUserPage />} />
 
 						<Route path='/*' element={<NoPage />} />
 					</Routes>
 				</main>
 				<Footer />
-				{/* <footer className='footer'></footer> */}
 			</BrowserRouter>
 		</>
 	);
